36fa63ff0a0b288aa7199bf971d58049216445cf
conorsullivan <40009692+conorsullivan@users.noreply.github.com>
2018-06-06 21:03:33 -0700

    Update glowingbroccoli.py

--- glowingbroccoli.py
+++ glowingbroccoli.py
@@ -4,5 +4,5 @@ This program will check the status of some websites'''
 
 import subprocess
 
-subprocess.check_output(["ping", "-c", "1", "google.cmm"])
+subprocess.check_output(["ping", "-c", "1", "google.com"])
 
